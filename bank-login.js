cd0fed2a82632f2e9bb647687d7ed6df2d8b9665
Barun Mitra <itbarunmitra1000@gmail.com>
2021-08-16 20:33:28 +0600

    create login page for banking site

---
+++ bank-login.js
@@ -0,0 +1,16 @@
+const loginButton = document.getElementById('login-button');
+loginButton.addEventListener('click',function(){
+    // console.log('clicked');
+    const loginInput = document.getElementById('login-input');
+    const loginInputValue = loginInput.value;
+
+    const loginPassword = document.getElementById('login-password');
+    const loginPasswordValue = loginPassword.value ;
+
+    if(loginInputValue == 'barun@gmail.com' && loginPasswordValue == 'password'){
+        window.location.href = 'banking.html';
+    }
+    else{
+      alert('Invalid email or password');
+    }
+});
