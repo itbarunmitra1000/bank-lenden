9d4ea6df23d2e5a411caa1e09a601e4e77dc0bae
Barun Mitra <itbarunmitra1000@gmail.com>
2021-08-16 23:49:57 +0600

    withdraw and blance part js

--- bank-lenden.js
+++ bank-lenden.js
@@ -6,14 +6,49 @@ depositButton.addEventListener('click', function () {
     const inputDepositAmountValue = inputDepositAmount.value;
     // console.log(inputDepositAmountValue);
 
+
     // adding input with total deposit money
     const totalDepositMoney = document.getElementById('total-deposit-money');
-     const currentDepositMoney = totalDepositMoney.innerText ;
+    const currentDepositMoney = totalDepositMoney.innerText;
 
     const addTotalDepositMoney = parseFloat(inputDepositAmountValue) + parseFloat(currentDepositMoney);
-    // addTotalDepositMoney.innerText = inputDepositAmountValue;
+
     totalDepositMoney.innerText = addTotalDepositMoney;
+
+    // add blance money 
+    const totalBalanceMoney = document.getElementById('total-balance-money');
+    const currentTotalBalanceMoney = totalBalanceMoney.innerText;
+    // console.log(currentTotalBalanceMoney);
+    const totalBalance = parseFloat(currentTotalBalanceMoney) + parseFloat(inputDepositAmountValue);
+    totalBalanceMoney.innerText = totalBalance;
 
     //blank input box
     inputDepositAmount.value = '';
+
 });
+
+// withdrawn part
+
+const withdrawButton = document.getElementById('withdraw-button');
+withdrawButton.addEventListener('click', function () {
+    const inputWithdrawAmount = document.getElementById('input-withdraw-amount');
+    const inputWithdrawAmountValue = inputWithdrawAmount.value;
+
+    const totalWithDrawMoney = document.getElementById('total-withdraw-money');
+    const currentWithdrawMoney = totalWithDrawMoney.innerText;
+    const addTotalWithdrawMoney = parseFloat(inputWithdrawAmountValue) + parseFloat(currentWithdrawMoney);
+    totalWithDrawMoney.innerText = addTotalWithdrawMoney;
+
+
+
+    const totalBalanceMoney = document.getElementById('total-balance-money');
+    const currentTotalBalanceMoney = totalBalanceMoney.innerText;
+    const totalBalance = parseFloat(currentTotalBalanceMoney) - parseFloat(inputWithdrawAmountValue);
+    totalBalanceMoney.innerText = totalBalance;
+
+    // blank input box 
+    inputWithdrawAmount.value = '';
+
+
+
+})
